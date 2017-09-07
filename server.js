3f92bf03098a40f990f554b472891627d3b2f41a
michellehodges <mischamangan@gmail.com>
2017-09-07 16:23:01 -0400

    More bug fixes on Sequelize creates.

--- server.js
+++ server.js
@@ -84,96 +84,100 @@ Like.belongsTo(User, { foreignKey: 'user_id' })
 Like.belongsTo(Message, { foreignKey: 'message_id' });
 
 User.sync()
+  .then(function() {
+    // return
+    // console.log('User models synced')
+    let users = [User.create({
+      id: '0e3374d2-4b90-4123-bbdc-41245c800215',
+      username: 'mindykaling',
+      password: 'donuts',
+    }),
+    User.create({
+      id: '042f9350-8e32-4cad-85ad-a87238f32007',
+      username: 'harrystyles',
+      password: 'taytay',
+    }),
+    User.create({
+      id: 'c9ef1aff-cc7e-4f71-8380-e33ff19c13da',
+      username: 'taylorswift',
+      password: 'calvin',
+    }),
+    User.create({
+      id: 'a36e0e1d-8e74-47cd-977a-deb915e53887',
+      username: 'mikehodges',
+      password: 'michelle',
+    }),
+    User.create({
+      id: '4be57d1e-a310-4bd7-9c85-535531457797',
+      username: 'abc',
+      password: '123',
+    })];
+
+    return Promise.all(users);
+  })
   .then(function(){
-    return
-    console.log('User models synced')
-    // User.create({
-    //   id: '0e3374d2-4b90-4123-bbdc-41245c800215',
-    //   username: 'mindykaling',
-    //   password: 'donuts',
-    // });
-    // User.create({
-    //   id: '042f9350-8e32-4cad-85ad-a87238f32007',
-    //   username: 'harrystyles',
-    //   password: 'taytay',
-    // });
-    // User.create({
-    //   id: 'c9ef1aff-cc7e-4f71-8380-e33ff19c13da',
-    //   username: 'taylorswift',
-    //   password: 'calvin',
-    // });
-    // User.create({
-    //   id: 'a36e0e1d-8e74-47cd-977a-deb915e53887',
-    //   username: 'mikehodges',
-    //   password: 'michelle',
-    // });
-    // User.create({
-    //   id: '4be57d1e-a310-4bd7-9c85-535531457797',
-    //   username: 'abc',
-    //   password: '123',
-    // })
-  .then(function(){
-    Message.sync().then(function(){
-      console.log('Message models synced')
-      // Message.create({
-      //   id: '4548d4c6-a337-46c2-9f23-459df8e3679b',
-      //   user_id: '4be57d1e-a310-4bd7-9c85-535531457797',
-      //   content: 'It is one of those days where all I want is a donut topped with sprinkles.',
-      // });
-      // Message.create({
-      //   id: 'd8a7daf7-ed18-4cd8-82fe-b378059b05c9',
-      //   user_id: 'a36e0e1d-8e74-47cd-977a-deb915e53887',
-      //   content: 'Gabble is such an awesome app!',
-      // });
-      // Message.create({
-      //   id: '5f0c7f0b-2336-4f47-baa9-24d5ad1fdc21',
-      //   user_id: 'c9ef1aff-cc7e-4f71-8380-e33ff19c13da',
-      //   content: 'I got my exam back the other day. It was a B. My mom will be angry.',
-      // });
-      // Message.create({
-      //   id: '3ac71891-deec-4870-b45d-d1422a479ae5',
-      //   user_id: 'c9ef1aff-cc7e-4f71-8380-e33ff19c13da',
-      //   content: 'The government... Sucks.',
-      // });
-      // Message.create({
-      //   id: '8eb2e1b7-f81e-4f0d-aba4-ae4aa2121c1c',
-      //   user_id: '0e3374d2-4b90-4123-bbdc-41245c800215',
-      //   content: 'Political comment here! Political commment there! Ridiculous!',
-      // });
+    return Message.sync().then(function() {
+      console.log('Message models synced');
+      let messages = [Message.create({
+        id: '4548d4c6-a337-46c2-9f23-459df8e3679b',
+        user_id: '4be57d1e-a310-4bd7-9c85-535531457797',
+        content: 'It is one of those days where all I want is a donut topped with sprinkles.',
+      }),
+      Message.create({
+        id: 'd8a7daf7-ed18-4cd8-82fe-b378059b05c9',
+        user_id: 'a36e0e1d-8e74-47cd-977a-deb915e53887',
+        content: 'Gabble is such an awesome app!',
+      }),
+      Message.create({
+        id: '5f0c7f0b-2336-4f47-baa9-24d5ad1fdc21',
+        user_id: 'c9ef1aff-cc7e-4f71-8380-e33ff19c13da',
+        content: 'I got my exam back the other day. It was a B. My mom will be angry.',
+      }),
+      Message.create({
+        id: '3ac71891-deec-4870-b45d-d1422a479ae5',
+        user_id: 'c9ef1aff-cc7e-4f71-8380-e33ff19c13da',
+        content: 'The government... Sucks.',
+      }),
+      Message.create({
+        id: '8eb2e1b7-f81e-4f0d-aba4-ae4aa2121c1c',
+        user_id: '0e3374d2-4b90-4123-bbdc-41245c800215',
+        content: 'Political comment here! Political commment there! Ridiculous!',
+      })];
+
+      return Promise.all(messages);
     })
   })
     .then(function(){
       Like.sync().then(function(){
         console.log('Like models synced')
-        // Like.create({
-        //   id: '37519323-dcf9-41f5-9ed4-adc14f12bd62',
-        //   user_id: 'a36e0e1d-8e74-47cd-977a-deb915e53887',
-        //   message_id: '8eb2e1b7-f81e-4f0d-aba4-ae4aa2121c1c',
-        // });
-        // Like.create({
-        //   id: '36181b04-07fa-48d7-94fe-3ad7eff5833e',
-        //   user_id: '042f9350-8e32-4cad-85ad-a87238f32007',
-        //   message_id: '8eb2e1b7-f81e-4f0d-aba4-ae4aa2121c1c',
-        // });
-        // Like.create({
-        //   id: 'b85f54dd-422e-43eb-bc11-a09bff714e52',
-        //   user_id: '0e3374d2-4b90-4123-bbdc-41245c800215',
-        //   message_id: '3ac71891-deec-4870-b45d-d1422a479ae5',
-        // });
-        // Like.create({
-        //   id: 'db08ce7a-69af-42f6-82c4-9257f760a922',
-        //   user_id: '4be57d1e-a310-4bd7-9c85-535531457797',
-        //   message_id: '4548d4c6-a337-46c2-9f23-459df8e3679b',
-        // });
-        // Like.create({
-        //   id: '625b4b29-b5f5-4d2c-a3eb-eb3df12e2a15',
-        //   user_id: 'c9ef1aff-cc7e-4f71-8380-e33ff19c13da',
-        //   message_id: '5f0c7f0b-2336-4f47-baa9-24d5ad1fdc21',
-        // });
-      // })
+        Like.create({
+          id: '37519323-dcf9-41f5-9ed4-adc14f12bd62',
+          user_id: 'a36e0e1d-8e74-47cd-977a-deb915e53887',
+          message_id: '8eb2e1b7-f81e-4f0d-aba4-ae4aa2121c1c',
+        });
+        Like.create({
+          id: '36181b04-07fa-48d7-94fe-3ad7eff5833e',
+          user_id: '042f9350-8e32-4cad-85ad-a87238f32007',
+          message_id: '8eb2e1b7-f81e-4f0d-aba4-ae4aa2121c1c',
+        });
+        Like.create({
+          id: 'b85f54dd-422e-43eb-bc11-a09bff714e52',
+          user_id: '0e3374d2-4b90-4123-bbdc-41245c800215',
+          message_id: '3ac71891-deec-4870-b45d-d1422a479ae5',
+        });
+        Like.create({
+          id: 'db08ce7a-69af-42f6-82c4-9257f760a922',
+          user_id: '4be57d1e-a310-4bd7-9c85-535531457797',
+          message_id: '4548d4c6-a337-46c2-9f23-459df8e3679b',
+        });
+        Like.create({
+          id: '625b4b29-b5f5-4d2c-a3eb-eb3df12e2a15',
+          user_id: 'c9ef1aff-cc7e-4f71-8380-e33ff19c13da',
+          message_id: '5f0c7f0b-2336-4f47-baa9-24d5ad1fdc21',
+        });
+      })
     })
-  })
-})
+
 
 /************************ END OF SCHEMA ****************************/
 
@@ -247,7 +251,7 @@ server.post('/register', function(request, response) {
   let newUsername = request.body.newUsername;
   let newPassword = request.body.newPassword;
 
-  if (newUsername !== null && newPassword !== null) {
+  if (newUsername !== null && newPassword !== null && newUsername > 0 && newUsername < 20 && newPassword > 0 && newPassword < 20) {
     User.create({
       username: newUsername,
       password: newPassword
@@ -264,15 +268,14 @@ server.post('/register', function(request, response) {
 
 server.post('/create', function (request, response) {
   console.log(request.body);
-  if (request.body.body.length != 0)  {
+  if ((request.body.body.length > 0) && (request.body.body.length < 140))  {
     Message.create({
       user_id: request.session.who.id,
       content: request.body.body,
     })
       .then(function(newGabble){
         response.redirect('/main');
-      })
-      .catch(function(err){
+      }).catch(function(err){
         console.log(err)
       })
 
@@ -282,7 +285,7 @@ server.post('/create', function (request, response) {
           response.render('main', {
             gabbles: results,
             currentUser: request.session.who.username,
-            needBody: "A gabble is required to post."
+            needBody: "A gabble of 140 characters max is required to post."
             })
         })
     }
